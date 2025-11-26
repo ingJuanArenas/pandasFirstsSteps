cb789b00caa7bf9d7b4456113ce4dfef491d993d
ingJuanArenas <juanarenasing@gmail.com>
2025-11-26 15:04:46 -0500

    cleaning

--- p1.py
+++ p1.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv("data.csv")
-
-
+##inplace is used to modify the original dataframe without creating a copy
+df.dropna(inplace= True)
+## get the mean
+mean = df["Age"].mean()
+## replace empty values with mean
+df.fillna({"Age":mean}, inplace=True)
 print(df.head())
 print(df.describe())
+print(df.info())
+print(df.duplicated())
+df.corr()
